4e61d213284d3e50e950f4438b903713567d81f5
keyem <krstmvrgas04@gmail.com>
2025-01-22 15:39:46 +0800

    [USER] edit-user.blade.php

--- resources/views/evotar/livewire/manage-system-user/edit-user.blade.php
+++ resources/views/evotar/livewire/manage-system-user/edit-user.blade.php
@@ -29,7 +29,7 @@
             x-transition:leave="transition ease-in duration-200"
             x-transition:leave-start="opacity-100 transform scale-100"
             x-transition:leave-end="opacity-0 transform scale-90"
-            class="bg-white p-6 rounded shadow-md w-3/5"
+            class="bg-white p-6 rounded shadow-md w-2/5"
         >
 
             <div class="flex justify-between items-center mb-4 border-b border-gray-300 pb-2">
@@ -45,7 +45,105 @@
                 </button>
             </div>
 
+            <!-- Election Details-->
 
+            <form wire:submit.prevent="proceedToAccessRole">
+                    <div>
+                        <div class="flex space-x-4">
+                            <div class="w-full">
+                                <div class="mb-3 relative w-full"
+                                     x-data="{ isOpen: false }">
+                                    <label for="candidate_name" class="text-[10px] text-left font-normal block mb-1">
+                                        Name of User
+                                        <span class="text-[10px] font-light italic">
+                                            <a href="{{ route('admin.unregistered.admin') }}"
+                                               class="underline text-red-500">(Please click here if user is not a registered voter).
+                                            </a>
+                                        </span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="candidate_name"
+                                        placeholder="Search for a user"
+                                        class="border border-gray-300 text-xs rounded-lg px-4 py-2 w-full"
+                                        wire:model.live="search"
+                                        x-on:focus="isOpen = true"
+                                        x-on:blur="setTimeout(() => isOpen = false, 200)"
+                                        autocomplete="off"
+                                    />
+                                    <div x-show="isOpen && search.length > 0" @click.away="isOpen = false"
+                                         class="flex z-10 bg-white border border-gray-300 rounded-lg w-full max-h-[50px] overflow-auto mt-[5px] shadow-lg">
+                                        <div class="w-full">
+                                            @if (!empty($users))
+                                                @forelse ($users as $user)
+                                                    <div
+                                                        class="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                                                        wire:click="selectUser({{ $user->id }})"
+                                                        x-on:click="isOpen = false"
+                                                    >
+                                                        {{ $user->first_name }} {{ $user->middle_initial }}
+                                                        . {{ $user->last_name }}
+                                                        - {{ $user->year_level }} {{ $user->program->name }}
+                                                    </div>
+                                                @empty
+                                                    <li class="px-4 py-2 text-gray-500">No results found.</li>
+                                                @endforelse
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    @error('selectedUser')
+                                    <span class="text-red-500 text-[10px] italic">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3 relative w-full">
+                                    <p class="text-[12px] text-left font-semibold my-2">Account Information</p>
+                                    <div class="flex-1 mb-3">
+                                        <label for="username"
+                                               class="text-[10px] text-left font-natural px-2 block ">Username</label>
+                                        <input type="text" name="username" wire:model="username"
+                                               class="border border-gray-300 text-xs rounded-lg px-4 py-2 w-full">
+                                        @error('username')
+                                        <span class="text-red-500 text-[10px] italic">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="flex-1 mb-3">
+                                        <label for="password"
+                                               class="text-[10px] text-left font-natural px-2 block ">Password</label>
+                                        <input type="password" name="password" wire:model="password"
+                                               class="border border-gray-300 text-xs rounded-lg px-4 py-2 w-full">
+                                        @error('password')
+                                        <span class="text-red-500 text-[10px] italic">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="flex-1 mb-3">
+                                        <label for="confirm_password" class="text-[10px] text-left font-natural px-2 block ">Confirm
+                                            Password</label>
+                                        <input type="password" name="confirm_password" wire:model="confirm_password"
+                                               class="border border-gray-300 text-xs rounded-lg px-4 py-2 w-full">
+                                        @error('confirm_password')
+                                        <span class="text-red-500 text-[10px] italic">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div class="mt-6 pt-3 flex justify-end space-x-2">
+                            <button type="button"
+                                    class="bg-gray-300 text-gray-700 text-[12px] h-7 px-4 py-1 rounded shadow-md hover:bg-gray-400 justify-center text-center"
+                                    @click="open = false">
+                                Cancel
+                            </button>
+                            <button type="submit"
+                                    class="bg-black text-white px-6 py-1 h-7 rounded shadow-md hover:bg-gray-700 text-[12px] justify-center text-center">
+                                Proceed to Access Role and Permission
+                            </button>
+                        </div>
+                    </div>
+                </form>
         </div>
     </div>
 </div>
