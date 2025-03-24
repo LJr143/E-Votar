dc61d5278595b55866ed9346543908d54175e62f
JohnnySons <lorjohn143@gmail.com>
2025-03-24 15:13:53 +0800

    header, election error

--- app/Livewire/Timer/AdminDashboardTimer.php
+++ app/Livewire/Timer/AdminDashboardTimer.php
@@ -8,10 +8,11 @@ class AdminDashboardTimer extends Component
 {
     public $selectedElection;
 
-    public function mount($selectedElection)
+    public function mount($selectedElection = null) // Default to null
     {
         $this->selectedElection = $selectedElection;
     }
+
     public function render()
     {
         return view('evotar.livewire.timer.admin-dashboard-timer');
