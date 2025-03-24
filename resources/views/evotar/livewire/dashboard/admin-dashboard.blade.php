e5e0bda0772923fb050758870e86210c8103b3c6
JohnnySons <lorjohn143@gmail.com>
2025-03-24 15:26:37 +0800

    header, election error

--- resources/views/evotar/livewire/dashboard/admin-dashboard.blade.php
+++ resources/views/evotar/livewire/dashboard/admin-dashboard.blade.php
@@ -63,7 +63,34 @@
                         <livewire:timer.admin-dashboard-timer :selectedElection="$selectedElection"
                                                               wire:key="timer-election-{{ $selectedElection->id }}"/>
                     @else
-                        <p class="text-gray-500">No active election.</p>
+                        <div class="bg-black text-white p-4 rounded-lg shadow-md text-center min-h-[160px] ">
+                            <div class="flex justify-between items-center">
+                                <div class="flex items-center space-x-2">
+                                    <i class="fas fa-stopwatch text-white text-sm"></i>
+                                    <span class="text-sm font-bold text-white">No Election Available</span>
+                                </div>
+
+                            </div>
+                            <div class="tick" data-did-init="handleTickInit" data-credits="false">
+                                <div
+                                    data-repeat="true"
+                                    data-layout="horizontal fit"
+                                    data-transform="preset(d, h, m, s) -> delay"
+                                    class="tick-container">
+
+                                    <div class="tick-group">
+                                        <div data-key="value" data-repeat="true" data-transform="pad(00) -> split -> delay">
+                                            <span data-view="flip" class="tick-value"></span>
+                                        </div>
+                                        <span data-key="label" data-view="text" class="tick-label"></span>
+                                    </div>
+                                </div>
+                                <div class="tick-onended-message" style="display: none">
+                                    <p>Time's up</p>
+                                </div>
+                            </div>
+
+                        </div>
                     @endif
                 </div>
                 <div
