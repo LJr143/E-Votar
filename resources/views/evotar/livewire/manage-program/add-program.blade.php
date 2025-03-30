578d68f1c92d3ea61eb7ab2b4b427d68d6c02a35
Sweet Ang <sflang00005@usep.edu.ph>
2025-03-30 21:05:42 +0800

    manage-college (tabs responsive,remove filter button and import button)) manage-program (tabs responsive, remove filter button and import button)) manage-program-table (tabs responsive,remove filter button and import button) add-college (responsive, animation buttons) add-program (responsive, animation buttons) add-program-major (responsive, animation buttons) edit-college (responsive, animation buttons) edit-program (responsive, animation buttons) edit-program-major (responsive, animation buttons) delete-college (responsive, animation buttons) delete-program (responsive, animation buttons) delete-program-major (responsive, animation buttons)

--- resources/views/evotar/livewire/manage-program/add-program.blade.php
+++ resources/views/evotar/livewire/manage-program/add-program.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: false }" x-cloak @program-created.window="open = false">
     <!-- Trigger Button -->
     <button @click="open = true"
-            class="w-[100px] mr-2 rounded py-[6px] px-2 bg-black text-white text-[12px] hover:bg-gray-700">
+            class="w-[100px] mr-2 rounded py-[6px] px-2 bg-black text-white text-[12px]  hover:drop-shadow hover:bg-gray-700 hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
         Add Program
     </button>
     <!-- Modal -->
@@ -55,12 +55,12 @@
                     </div>
                     <div class="mt-6 pt-3 flex justify-end space-x-2">
                         <button type="button"
-                                class="bg-gray-300 text-gray-700 text-[12px] h-7 px-4 py-1 rounded shadow-md hover:bg-gray-400 justify-center text-center"
+                                class="bg-gray-300 text-gray-700 text-[12px] h-7 px-4 py-1 rounded shadow-md hover:bg-gray-400 justify-center text-center  hover:drop-shadow  hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110"
                                 @click="open = false">
                             Cancel
                         </button>
                         <button type="submit"
-                                class="bg-black text-white px-6 py-1 h-7 rounded shadow-md hover:bg-gray-700 text-[12px] justify-center text-center">
+                                class="bg-black text-white px-6 py-1 h-7 rounded shadow-md hover:bg-gray-700 text-[12px] justify-center text-center  hover:drop-shadow  hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
                             Add
                         </button>
                     </div>
--- resources/views/evotar/livewire/manage-program/delete-program.blade.php
+++ resources/views/evotar/livewire/manage-program/delete-program.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ open: false }" x-cloak @program-deleted.window="open = false">
     <!-- Trigger Button -->
     <button @click="open = true"
-            class="bg-white border border-gray-100 rounded p-1 w-[30px] flex-row  items-center justify-items-center">
+            class="bg-white border border-gray-100 rounded p-1 w-[30px] flex-row  items-center justify-items-center hover:drop-shadow hover:bg-gray-200 hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
         <svg width="15" height="17" viewBox="0 0 15 17" fill="none"
              xmlns="http://www.w3.org/2000/svg">
             <path
@@ -53,11 +53,11 @@
 
                     <div class="flex justify-end mt-4 space-x-2">
                         <button @click="open = false" type="button"
-                                class="px-4 py-2 border border-gray-300 text-gray-800 rounded-md shadow-md hover:bg-gray-400 text-xs">
+                                class="px-4 py-2 border border-gray-300 text-gray-800 rounded-md shadow-md hover:bg-gray-400 text-xs  hover:drop-shadow hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
                             Cancel
                         </button>
                         <button type="submit"
-                                class="px-4 py-2 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700 text-xs">
+                                class="px-4 py-2 bg-red-600 text-white rounded-md shadow-md hover:bg-red-700 text-xs  hover:drop-shadow hover:scale-105 hover:ease-in-out hover:duration-300 transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110">
                             Delete
                         </button>
                     </div>
