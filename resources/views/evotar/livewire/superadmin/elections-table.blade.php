a86839e90947fec946327ef76a479564899cf302
JohnnySons <lorjohn143@gmail.com>
2025-03-31 23:37:54 +0800

    minor changes - export to export list of Elections

--- resources/views/evotar/livewire/superadmin/elections-table.blade.php
+++ resources/views/evotar/livewire/superadmin/elections-table.blade.php
@@ -46,7 +46,7 @@
                                         <path
                                             d="M480-336 288-528l51-51 105 105v-342h72v342l105-105 51 51-192 192ZM263.72-192Q234-192 213-213.15T192-264v-72h72v72h432v-72h72v72q0 29.7-21.16 50.85Q725.68-192 695.96-192H263.72Z"/>
                                     </svg>
-                                    <span wire:loading.remove wire:target="exportElection" class="text-[12px]">Export</span>
+                                    <span wire:loading.remove wire:target="exportElection" class="text-[12px]">Export List of Elections</span>
                                     <svg wire:loading wire:target="exportElection" class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
                                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
                                                 stroke-width="4"></circle>
