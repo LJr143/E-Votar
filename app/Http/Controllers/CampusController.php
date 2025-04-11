05a033656775f63341aa0f1560c4c3bb7a793da1
JohnnySons <lorjohn143@gmail.com>
2025-04-11 11:47:59 +0800

    fix and add announcement.blade.php

--- app/Http/Controllers/CampusController.php
+++ app/Http/Controllers/CampusController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Campus;
-use App\Models\college;
+use App\Models\College;
 use App\Models\Program;
 use App\Models\program_major;
 use Illuminate\Http\Request;
