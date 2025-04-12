1b525b25b97b5f83807d58a78ace82f01c0f4ede
JohnnySons <lorjohn143@gmail.com>
2025-04-12 11:28:47 +0800

    fix url timer admin

--- resources/views/evotar/auth/facial-registration.blade.php
+++ resources/views/evotar/auth/facial-registration.blade.php
@@ -106,6 +106,8 @@
         </div>
         <p id="status-message">Initializing camera...</p>
         <button id="capture-button" data-user-id="{{ $user->id }}">Start Registration</button>
+        <script defer src="https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/dist/face-api.min.js"></script>
+
 
         <script src="{{ asset('storage/js/face-api.min.js') }}"></script>
         <script>
