1e3dcb9800a1e4f7aa628abf9f09e64bba7289ee
JohnnySons <lorjohn143@gmail.com>
2025-04-15 19:47:03 +0800

    fix automatic back up

--- app/Livewire/ManageDatabase/ManageDatabaseBackup.php
+++ app/Livewire/ManageDatabase/ManageDatabaseBackup.php
@@ -69,7 +69,7 @@ class ManageDatabaseBackup extends Component
                 'schedule_parameters' => $this->getScheduleParameters(),
                 'next_backup_at' => $nextBackup,
                 'last_backup_at' => null,
-                'created_by' => auth()->user()->id ?? 'scheduled back up',
+                'created_by' => auth()->user()->id ?? 34,
             ]);
 
             $this->resetForm();
