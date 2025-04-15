ab91996eaf65d26d941761c935e57a3d633f79ae
JohnnySons <lorjohn143@gmail.com>
2025-04-15 20:26:42 +0800

    fix automatic back up

--- app/Http/Middleware/CheckBlockedIp.php
+++ app/Http/Middleware/CheckBlockedIp.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Models\IpRecord;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,8 +13,9 @@ class CheckBlockedIp
     public function handle(Request $request, Closure $next)
     {
         $ipAddress = $request->ip();
+        $userId = Auth::id();
 
-        $ipRecord = IpRecord::where('ip_address', $ipAddress)
+        $ipRecord = IpRecord::where('ip_address', $ipAddress)->where('user_id', $userId)
             ->where('status', 'blocked')
             ->first();
 
