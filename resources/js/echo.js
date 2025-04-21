cd528457698aec39e46bfa53148175399c3ffb8a
JohnnySons <lorjohn143@gmail.com>
2025-04-22 04:55:13 +0800

    fix reverb

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,18 +6,21 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname, // Fixed
-    wsPort: import.meta.env.VITE_REVERB_PORT || (window.location.protocol === 'https:' ? 443 : 80), // Fixed
+    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+    wsPort: parseInt(import.meta.env.VITE_REVERB_PORT) || (window.location.protocol === 'https:' ? 443 : 80),
     wssPort: 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME === 'https'), // Fixed
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME === 'https'),
     enabledTransports: ['ws', 'wss'],
     disableStats: true,
-    authEndpoint: '/broadcasting/auth',
     auth: {
         headers: {
-            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.content,
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.content || '',
+            'X-Requested-With': 'XMLHttpRequest'
         },
     },
+    cluster: import.meta.env.VITE_REVERB_CLUSTER || 'mt1',
+    activityTimeout: 60000, // 60 seconds
+    pongTimeout: 30000, // 30 seconds
 });
 
 
