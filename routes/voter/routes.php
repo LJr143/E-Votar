a3da4817b17268bc1f3a610a9d0bad761a381fc4
JohnnySons <lorjohn143@gmail.com>
2025-05-16 10:55:07 +0800

    fixing forgot password

--- routes/voter/routes.php
+++ routes/voter/routes.php
@@ -12,12 +12,12 @@ Route::group(['middleware' => ['superadmin.check', 'redirect.auth']], function (
 });
 
 // Voter Protected Routes
-Route::middleware(['splash.screen', 'check.deactivated', 'single.voter.session', 'check.blocked.ip', 'track.ip.user'])->prefix('voter')->group(function () {
+Route::middleware(['splash.screen', 'check.deactivated', 'check.blocked.ip', 'track.ip.user'])->prefix('voter')->group(function () {
     Route::get('available/election', [VoterElectionController::class, 'voterElectionRedirect'])->name('voter.election.redirect')->middleware('voter.auth');
     Route::get('/dashboard/{slug}', [VoterElectionController::class, 'voterDashboard'])->name('dashboard')->middleware(['voter.auth', 'voter.access']);
     Route::get('step-1-tutorial', [ViewController::class, 'step1Tutorial'])->name('voter.step1')->middleware('voter.auth' );
     Route::get('step-2-tutorial', [ViewController::class, 'step2Tutorial'])->name('voter.step2')->middleware('voter.auth' );
-    Route::get('/voting-process/{slug}', [VoterElectionController::class, 'voting'])->name('voter.voting')->middleware(['voter.auth' , 'verified.voter', 'vote.checker', 'election.status']);
+    Route::get('/voting-process/{slug}', [VoterElectionController::class, 'voting'])->name('voter.voting')->middleware(['voter.auth' , 'verified.voter', 'vote.checker']);
     Route::get('/voting/vote-submission', [VoterElectionController::class, 'confirmVoting'])->name('voter.voting.confirm')->middleware('voter.auth' );
     Route::get('/accounts-settings', [ViewController::class, 'voterAccountSettings'])    ->name('voter.account.settings')->middleware('voter.auth' );
     Route::get('/verify-vote', [VoterElectionController::class, 'showVerifyVotePage']) ->name('verify.vote.page')->middleware('auth');
