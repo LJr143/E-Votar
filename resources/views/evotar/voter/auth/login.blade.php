1ac7ba4bc132598ad7a4e678afc56dbbba3f785f
JohnnySons <lorjohn143@gmail.com>
2025-05-16 17:22:46 +0800

    fixing forgot password

--- resources/views/evotar/voter/auth/login.blade.php
+++ resources/views/evotar/voter/auth/login.blade.php
@@ -102,12 +102,12 @@
                                     </button>
 
                                     <div class="flex justify-start items-left mt-4">
-                                        @if (Route::has('password.request'))
+{{--                                        @if (Route::has('password.request'))--}}
                                             <a class="underline text-[11px] text-gray-700 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                                href="{{ route('password.request') }}">
                                                 {{ __('Forgot your password?') }}
                                             </a>
-                                        @endif
+{{--                                        @endif--}}
                                     </div>
                                 </div>
                             </form>
