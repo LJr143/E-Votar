57489172c83573e4b921707e14c531e63fff41af
JohnnySons <lorjohn143@gmail.com>
2025-05-17 22:55:19 +0800

    fixing vote tally

--- resources/views/evotar/livewire/selected-election/vote-tally-in-website.blade.php
+++ resources/views/evotar/livewire/selected-election/vote-tally-in-website.blade.php
@@ -101,10 +101,13 @@
                         // For multiple winners, calculate partial abstentions
                         if ($positionWinnersCount > 1) {
                             $distinctVoters = DB::table('votes')
+                             ->join('users', 'votes.user_id', '=', 'users.id')
                                 ->where('election_id', $this->selectedElection)
-                                ->where('position_id', $positionId)
-                                ->distinct('user_id')
-                                ->count('user_id');
+                                 ->where('position_id', $positionId)
+                                    ->where('votes.election_id', $this->selectedElection)
+                                    ->where('programs.council_id', $council->id)
+                                    ->distinct('votes.user_id')
+                                    ->count('votes.user_id');
 
                             $expectedVotes = $distinctVoters * $positionWinnersCount;
                             $partialAbstain = $expectedVotes - $positionTotalVotes;
