7009709187c8520cd45b5dd83a799d9295fa698d
JohnnySons <lorjohn143@gmail.com>
2025-06-17 11:57:34 +0800

    edit-voter.blade.php

--- resources/views/evotar/livewire/manage-voter/edit-voter.blade.php
+++ resources/views/evotar/livewire/manage-voter/edit-voter.blade.php
@@ -118,8 +118,8 @@
                             <select name="gender" id="gender" wire:model="gender"
                                     class="block appearance-none border border-gray-300 text-xs rounded px-2 py-0 w-full focus:ring-black focus:border-black h-[28px] text-[12px] @error('gender') border-red-500 @enderror">
                                 <option value="" disabled selected>Select your gender</option>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
                                 <option value="non-binary">Non-binary</option>
                                 <option value="prefer-not-to-say">Prefer not to say</option>
                             </select>
