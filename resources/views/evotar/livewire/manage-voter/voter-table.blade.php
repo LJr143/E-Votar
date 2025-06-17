4ef49dee0f863e1fef2d45ff03c6657d637dcc75
JohnnySons <lorjohn143@gmail.com>
2025-06-17 09:41:52 +0800

    DashboardController.php

--- resources/views/evotar/livewire/manage-voter/voter-table.blade.php
+++ resources/views/evotar/livewire/manage-voter/voter-table.blade.php
@@ -335,7 +335,7 @@
                                     </td>
                                     <td class="py-3 px-6 text-center">
                                             <span class="inline-block w-auto px-2 py-1 text-center whitespace-nowrap rounded
-                                                @if($voter->votes->isNotEmpty()) bg-purple-500 text-white @else bg-gray-500 text-white @endif">
+                                                @if($voter->votes->isNotEmpty() || $voter->abstained->isNotEmpty()) bg-purple-500 text-white @else bg-gray-500 text-white @endif">
                                                 @if($voter->votes->isNotEmpty() || $voter->abstained->isNotEmpty()) Voted @else Not Voted @endif
                                             </span>
                                     </td>
